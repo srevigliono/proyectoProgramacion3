0a17ad372e2d93f4e41853eedeaf4d77c7742060
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-11-21 23:41:24 -0300

    empezando home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,92 @@
-import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { db } from '../firebase/config';
 
-export default function Home() {
-  return (
-    <SafeAreaView>
-      <View>
-        <Text>Pantalla de Home</Text>
+export default class HomeMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    db.collection('posts')
+      .orderBy('createdAt')
+      .onSnapshot((docs) => {
+        let posts = [];
+        docs.forEach((doc) => {
+          posts.push({
+            data: doc.data(),
+          });
+        });
+        this.setState({
+          posts: posts,
+          loading: false,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Posts</Text>
+
+        {!this.state.loading && (
+          <FlatList
+            data={this.state.posts}
+            renderItem={({ item }) => (
+              <View style={styles.postContainer}>
+                <Text style={styles.postText}>{item.data.text}</Text>
+                <Text style={styles.postUser}>{item.data.user}</Text>
+                <Text style={styles.postDate}>
+                {new Date(item.data.createdAt).toLocaleString()}
+                </Text>
+              </View>
+            )}
+          />
+        )}
       </View>
-    </SafeAreaView>
-  );
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+    color: '#333',
+  },
+  postContainer: {
+    marginBottom: 20,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 10,
+    backgroundColor: '#f9f9f9',
+  },
+  postText: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: '#333',
+  },
+  postUser: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    color: '#555',
+  },
+  postDate: {
+    fontSize: 14,
+    color: '#888',
+  },
+});
